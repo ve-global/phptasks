dd62e4dce1f67c739a17fb6e7d42ec4faf39a087
stevewest <uruwolf@gmail.com>
2014-06-11 18:34:20 +0100

    Adds sample method and test

---
+++ src/Math.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace PHPTasks;
+
+/**
+ * Contains some simple math functions
+ */
+class Math
+{
+	/**
+	 * Adds two numbers.
+	 *
+	 * @param  int $a
+	 * @param  int $b
+	 * @return int
+	 */
+	public function addNumbers($a, $b)
+	{
+		return $a + $b;
+	}
+
+}
+
